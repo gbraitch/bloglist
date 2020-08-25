b4e77ed0e02c3f6a3c16e106e9be3187401be7d2
Gursewak Braitch <g.braitch@hotmail.com>
2020-08-24 20:21:58 -0700

    Setting up heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -159,12 +159,12 @@ const App = () => {
 
   const switchTheme = (e) => {
     if (e.target.checked) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
+      document.documentElement.setAttribute('data-theme', 'dark')
+      localStorage.setItem('theme', 'dark')
     }
     else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      localStorage.setItem('theme', 'light');
+      document.documentElement.setAttribute('data-theme', 'light')
+      localStorage.setItem('theme', 'light')
     }
   }
 
